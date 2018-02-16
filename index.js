ec9bf185d174b358ad154bd54989bdb206e057dc
Lisa Chan <xlisachan@gmail.com>
2018-02-16 15:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(deliLine){
+  var name;
     if (deliLine.length === 0) {
       return 'There is nobody waiting to be served!';
     } else {
