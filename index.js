6a45d48a3f0adf34b2df6c288b6c900e04c7fecf
Lisa Chan <xlisachan@gmail.com>
2018-02-16 14:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function takeANumber(katzDeliLine, name) {
   var position = katzDeliLine.length + 1;
   katzDeliLine.push(name);
-
   return `Welcome, ${name}. You are number ${position} in line.`;
+}
+
+function nowServing(){
+  var name;
+  for (let i = 0; i < deliLine.length; i++) {
+    console.log(`Currently serving ${name}.`)
+    deliLine.shift();
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
 }
