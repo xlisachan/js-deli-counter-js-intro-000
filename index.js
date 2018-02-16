fc3214e0db63587cea60996ee3ab1f269a3f15c0
Lisa Chan <xlisachan@gmail.com>
2018-02-16 16:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(deliLine){
     if (deliLine.length == 0) {
       return 'There is nobody waiting to be served!';
     } else {
-      return `Currently serving ${deliLine[name]}.`;
+      return `Currently serving ${deliLine[0]}.`;
     }
 
 }
