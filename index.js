9b70102d31b93d43d37c10c1a2d556e81ba997c7
Lisa Chan <xlisachan@gmail.com>
2018-02-16 16:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,21 +15,3 @@ function nowServing(deliLine){
 
 }
 
-
-  it('returns an announcement about the person it is serving, and shifts the line', () => {
-    const deliLine = ["Steven", "Blake", "Avi"]
-    expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
-    expect(deliLine).toEqual(["Blake", "Avi"]);
-  });
-});
-
-describe('currentLine(line)', () => {
-  it('returns "The line is currently empty." if no one is in line', () => {
-    expect(currentLine([])).toEqual("The line is currently empty.");
-  });
-
-  it('says who is in line when there are people waiting', () => {
-    expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
-  });
-});
-})
