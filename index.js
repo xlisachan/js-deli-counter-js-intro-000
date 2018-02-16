4fcac4fcdb9e9e095f07dd12df7dac9cac6687a8
Lisa Chan <xlisachan@gmail.com>
2018-02-16 14:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function takeANumber(katzDeliLine, name) {
   var katzDeliLine = [];
+  var position = katzDeliLine.length + 1;
   katzDeliLine = Object.assign({}, name);
+
   return `Welcome, ${name}. You are number ${position} in line.`;
 }
